8034a3274ff1683ce160c8b6424c36d29d57ed93
Aistis Stankevicius <a.stankevicius@greenrunning.com>
2018-03-02 17:36:40 +0000

    Initial commit

--- pyGPGO/logger.py
+++ pyGPGO/logger.py
@@ -1,3 +1,6 @@
+import logging
+
+
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -8,23 +11,51 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+
 class EventLogger:
-    def __init__(self, gpgo):
+    def __init__(self, gpgo, enable_logging=True, log_file=None):
         self.gpgo = gpgo
+        self.enabled = enable_logging
+
         self.header = 'Evaluation \t Proposed point \t  Current eval. \t Best eval.'
-        self.template = '{:6} \t {}. \t  {:6} \t {:6}'
-        print(self.header)
+        self.template = '{0: <6} \t {0: <6}. \t  {0: <6} \t {0: <6}'
+
+        if self.enabled:
+            # Create logger
+            logging.getLogger('root').setLevel(logging.INFO)
+
+            self._logger = logging.getLogger('pyGPGO')
+            self._logger.setLevel(logging.INFO)
+            formatter = logging.Formatter('%(message)s')
+
+            # create console handler with a higher log level
+            ch = logging.StreamHandler()
+            ch.setLevel(logging.INFO)
+            # create formatter and add it to the handlers
+            ch.setFormatter(formatter)
+            # add the handlers to the logger
+            self._logger.addHandler(ch)
+
+            if log_file is not None:
+                # create file handler which logs even debug messages
+                fh = logging.FileHandler('pygpgo.log')
+                fh.setLevel(logging.DEBUG)
+                fh.setFormatter(formatter)
+                self._logger.addHandler(fh)
 
     def _printCurrent(self, gpgo):
-        eval = str(len(gpgo.GP.y) - gpgo.init_evals)
-        proposed = str(gpgo.best)
-        curr_eval = str(gpgo.GP.y[-1])
-        curr_best = str(gpgo.tau)
-        if float(curr_eval) >= float(curr_best):
-            curr_eval = bcolors.OKGREEN + curr_eval + bcolors.ENDC
-        print(self.template.format(eval, proposed, curr_eval, curr_best))
+        if self.enabled:
+            eval = str(len(gpgo.GP.y) - gpgo.init_evals)
+            proposed = str(gpgo.best)
+            curr_eval = str(gpgo.GP.y[-1])
+            curr_best = str(gpgo.tau)
+            if float(curr_eval) >= float(curr_best):
+                curr_eval = bcolors.OKGREEN + curr_eval + bcolors.ENDC
+            self._logger.info(self.template.format(eval, proposed, curr_eval, curr_best))
 
     def _printInit(self, gpgo):
-        for init_eval in range(gpgo.init_evals):
-            print(self.template.format('init', gpgo.GP.X[init_eval], gpgo.GP.y[init_eval], gpgo.tau))
+        if self.enabled:
+            self._logger.info(self.header)
+            for init_eval in range(gpgo.init_evals):
+                self._logger.info(self.template.format('init', gpgo.GP.X[init_eval], gpgo.GP.y[init_eval], gpgo.tau))
 
